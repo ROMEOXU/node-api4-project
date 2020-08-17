24df1b717a789386c64a9992708db5a4bf3ac59b
Mac <xuromeo@gmail.com>
2020-08-16 17:45:55 -0700

    up

---
+++ index.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 3000;
+const data = require('./data.json');
+app.use(express.json());
+function middlewaretest(){
+    return (req,res,next)=>{
+        console.log('i am middleware here');
+        next()
+    }
+}
+app.get('/',middlewaretest(),(req,res)=>{
+    res.send('hello ,i am romeo')
+})
+
+app.get('/member',(req,res)=>{
+    res.send(data)
+})
+
+app.listen(port,()=>{
+    console.log(`listening on http://localhost:${port}`)
+})
